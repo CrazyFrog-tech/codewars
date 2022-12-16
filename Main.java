eda208dc3bdc4cad0e5d16b81e7e074cdbe7f7c1
allana <mohamad.allan@rigd-loxia.nl>
2022-12-16 14:31:44 +0100

    hallo

--- Main.java
+++ Main.java
@@ -5,44 +5,42 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void mains(String[] args) {
 
     }
 
-    public static int[][] matrixMultiplication(int[][] a, int[][] b) {
-        int row1 = 4, col1 = 3, row2 = 3, col2 = 4;
-//        printValues(a);
-//        printValues(b);
+    static void printMatrix(int M[][],
+        int rowSize,
+        int colSize)
+    {
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < colSize; j++)
+                System.out.print(M[i][j] + " ");
 
-        return multiplyMatrix(row1, col1, a,
-                row2, col2, b);
-    }
-
-    private static void printValues(int[][] a) {
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[i].length; j++) {
-                System.out.println("Values at arr["+i+"]["+j+"] is "+ a[i][j]);
-            }
-
+            System.out.println();
         }
     }
 
-    static int[][] multiplyMatrix(
-            int row1, int col1, int A[][],
-            int row2, int col2, int B[][])
+    // Function to multiply
+    // two matrices A[][] and B[][]
+    static void multiplyMatrix(
+        int row1, int col1, int A[][],
+        int row2, int col2, int B[][])
     {
         int i, j, k;
 
         // Print the matrices A and B
         System.out.println("\nMatrix A:");
+        printMatrix(A, row1, col1);
         System.out.println("\nMatrix B:");
+        printMatrix(B, row2, col2);
 
         // Check if multiplication is Possible
         if (row2 != col1) {
 
             System.out.println(
-                    "\nMultiplication Not Possible");
-            return null;
+                "\nMultiplication Not Possible");
+            return;
         }
 
         // Matrix to store the result
@@ -60,7 +58,26 @@ public class Main {
 
         // Print the result
         System.out.println("\nResultant Matrix:");
-        return C;
+        printMatrix(C, row1, col2);
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+
+        int row1 = 4, col1 = 3, row2 = 3, col2 = 4;
+
+        int A[][] = { { 1, 1, 1 },
+            { 2, 2, 2 },
+            { 3, 3, 3 },
+            { 4, 4, 4 } };
+
+        int B[][] = { { 1, 1, 1, 1 },
+            { 2, 2, 2, 2 },
+            { 3, 3, 3, 3 } };
+
+        multiplyMatrix(row1, col1, A,
+            row2, col2, B);
     }
     public static int[][] multiply(int[][] A, int[][] B, int rowA, int colA,
                                    int rowB, int colB, int size) {
