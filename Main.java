e72d38b6713426ffb23bce4614c280399679fe0d
allana <mohamad.allan@rigd-loxia.nl>
2023-02-14 13:33:10 +0100

    hallo

--- Main.java
+++ Main.java
@@ -9,6 +9,20 @@ public class Main {
     }
 
     public static int longestSlideDown(int[][] pyramid) {
+        int sum = pyramid[0][0];
+        int index = 0;
+
+        for (int i = 1; i < pyramid.length; i++) {
+            if(pyramid[i][index + 1] > pyramid[i][index] ){
+                index = index + 1;
+
+            }
+             sum += pyramid[i][index];
+
+        }
+
+
+        return sum;
         // Code Goes Here..
     }
 
