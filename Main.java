d9989362a7a289b0e0160dbf497a44f70a2c5dac
allana <mohamad.allan@rigd-loxia.nl>
2023-02-14 11:56:16 +0100

    hallo

--- Main.java
+++ Main.java
@@ -1,60 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void mains(String[] args) {
-
-    }
-
-
-    // Function to multiply
-    // two matrices A[][] and B[][]
-    static int[][] multiplyMatrix(
-        int row1, int col1, int[][] A,
-        int row2, int col2, int[][] B)
-    {
-        int i, j, k;
-
-        // Check if multiplication is Possible
-        if ( col1 != row2) {
-
-            System.out.println(
-                "\nMultiplication Not Possible");
-            return A;
-        }
-
-        int C[][] = new int[row1][col2];
-
-        for (i = 0; i < row1; i++) {
-            for (j = 0; j < col2; j++) {
-                for (k = 0; k < row2; k++)
-                    C[i][j] += A[i][k] * B[k][j];
-            }
-        }
-
-        return C;
-    }
-
     // Driver code
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
-
-
-        int A[][] = { { 1, 1, 1 },
-            { 2, 2, 2 },
-            { 3, 3, 3 },
-            { 4, 4, 4 } };
-
-        int B[][] = { { 1, 1, 1, 1 },
-            { 2, 2, 2, 2 },
-            { 3, 3, 3, 3 } };
-        int row1 = 4, col1 = 3, row2 = 3, col2 = 4;
-        System.out.println(A[0].length + "A length");
-
-        multiplyMatrix(row1, col1, A,
-            row2, col2, B);
     }
 
-    public static int[][] matrixMultiplication(int[][] a, int[][] b) {
-        return multiplyMatrix(a.length, a[0].length, a, b.length, b[0].length, b);
+    public static int longestSlideDown(int[][] pyramid) {
+        // Code Goes Here..
     }
+
 }
--- MainTest.java
+++ MainTest.java
@@ -1,70 +1,26 @@
-
+import static org.testng.AssertJUnit.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MainTest {
-
     @Test
-    public void exampleTest() {
-
-        int[][] a = {
-                {1,2},
-                {3, 2}};
-
-        int[][] b = {
-                {3,2},
-                {1, 1}};
-
-        int[][] expected = {
-                {5, 4},
-                {11, 8}};
-
-        int[][] actual = Main.matrixMultiplication(a, b);
-        assertArrayEquals(expected, actual);
+    public void test() {
+        int[][] test = new int[][]{{75},
+                {95, 64},
+                {17, 47, 82},
+                {18, 35, 87, 10},
+                {20, 4, 82, 47, 65},
+                {19, 1, 23, 75, 3, 34},
+                {88, 2, 77, 73, 7, 63, 67},
+                {99, 65, 4, 28, 6, 16, 70, 92},
+                {41, 41, 26, 56, 83, 40, 80, 70, 33},
+                {41, 48, 72, 33, 47, 32, 37, 16, 94, 29},
+                {53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14},
+                {70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57},
+                {91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48},
+                {63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31},
+                {4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23},
+        };
+        assertEquals(1074, Main.longestSlideDown(test));
     }
-
-    @Test
-    public void basicTest() {
-
-        {
-            int[][] a = {
-                    { 9, 7 },
-                    { 0, 1 }};
-
-            int[][] b = {
-                    { 1, 1 },
-                    { 4, 12 }};
-
-            int[][] expected = {
-                    { 37, 93 },
-                    { 4, 12 }};
-
-            int[][] actual = Main.matrixMultiplication(a, b);
-            assertArrayEquals(expected, actual);
-        }
-
-        {
-
-            int[][] a = {
-                    { 1, 2, 3 },
-                    { 3, 2, 1 },
-                    { 2, 1, 3 }};
-
-            int[][] b = {
-                    { 4, 5, 6 },
-                    { 6, 5, 4 },
-                    { 4, 6, 5 }};
-
-            int[][] expected = {
-                    { 28, 33, 29 },
-                    { 28, 31, 31 },
-                    { 26, 33, 31 }};
-
-            int[][] actual = Main.matrixMultiplication(a, b);
-            assertArrayEquals(expected, actual);
-        }
-    }
-
 }
