da2ce70e9093d4d480db0cc3917f3c14624e41e9
CrayzFrog-tech <allanmohamad7@gmail.com>
2023-07-02 11:58:14 +0200

    commit

--- Main.java
+++ Main.java
@@ -12,82 +12,82 @@ public class Main {
 
     // Driver code
     public static int[] snail(int[][] array) {
-        boolean goingRight = false;
-        boolean goingLeft = false;
-        boolean goingDown =false;
-        boolean goingUp = false;
-        int horizontalIndex = 0;
-        int verticalIndex = 0;
-
-        int verticalArrayLength = array.length;
-        int totalSize = array.length * array[0].length;
-        boolean firstTime = false;
-        for (int rowNumber = verticalIndex; rowNumber < verticalArrayLength; rowNumber++) {
-            if (rowNumber > 0) {
-                firstTime = true;
-            }
-            int horizontalArrayLength = array[rowNumber].length;
-            for (int columnNumber = horizontalIndex; columnNumber < horizontalArrayLength; columnNumber++) {
-                int number = array[rowNumber][columnNumber];
-                int horizontalArraySize = horizontalArrayLength - 1;
-                int verticalArraySize = verticalArrayLength - 1;
-                String key = "" + rowNumber + columnNumber;
-                int keyInt = Integer.parseInt(key);
-
-                if (
-                        !keysList.contains(keyInt)) {
-                    sortedList.add(number);
-                    keysList.add(keyInt);
-
-                }
-                if (columnNumber == horizontalArraySize || !keysList.contains(keyInt)) {
-
-                    if(columnNumber == horizontalArraySize){
-                        horizontalIndex = columnNumber;
-                    }else {
-                        horizontalIndex = 0;
-                    }
-                    columnNumber = horizontalArraySize;
-                    if(keysList.contains(keyInt)){
-                        horizontalIndex =0;
-                    }
-
-                }
-                if (rowNumber == verticalArraySize && columnNumber != 0) {
-                    horizontalIndex = columnNumber - 1;
-                    rowNumber--;
-                    break;
-
-                }
-                if (columnNumber == 0 && firstTime) {
-
-                    int newRowNumber = rowNumber - 2;
-                    int rownumberGood = rowNumber - 1;
-                    String key1 = "" + rownumberGood + columnNumber;
-                    int keyInt1 = Integer.parseInt(key1);
-                    if(!keysList.contains(keyInt1)){
-                        rowNumber = newRowNumber;
-                        verticalIndex = rowNumber;
-                        horizontalIndex = 0;
-                        firstTime = false;
-                        break;
-                    }
-
-
-                }
-                if (sortedList.size() == totalSize) {
-                    break;
-                }
-            }
-            if (sortedList.size() == totalSize) {
-                break;
-            }
-
+        int rows = array.length;
+        if (rows == 0) {
+            return new int[0];
         }
-        // enjoy
-        return sortedList.stream().mapToInt(i -> i).toArray();
+
+        int columns = array[0].length;
+        int totalSize = rows * columns;
+        int[] sortedArray = new int[totalSize];
+
+        int top = 0, bottom = rows - 1, left = 0, right = columns - 1;
+        int index = 0;
+
+        while (index < totalSize) {
+            // Traverse right
+            for (int i = left; i <= right && index < totalSize; i++) {
+                sortedArray[index++] = array[top][i];
+            }
+            top++;
+
+            // Traverse down
+            for (int i = top; i <= bottom && index < totalSize; i++) {
+                sortedArray[index++] = array[i][right];
+            }
+            right--;
+
+            // Traverse left
+            for (int i = right; i >= left && index < totalSize; i--) {
+                sortedArray[index++] = array[bottom][i];
+            }
+            bottom--;
+
+            // Traverse up
+            for (int i = bottom; i >= top && index < totalSize; i--) {
+                sortedArray[index++] = array[i][left];
+            }
+            left++;
+        }
+
+        return sortedArray;
     }
 
 
 }
 
+//                if (columnNumber == horizontalArraySize || !keysList.contains(keyInt)) {
+//
+//                    if(columnNumber == horizontalArraySize){
+//                        horizontalIndex = columnNumber;
+//                    }else {
+//                        horizontalIndex = 0;
+//                    }
+//                    columnNumber = horizontalArraySize;
+//                    if(keysList.contains(keyInt)){
+//                        horizontalIndex =0;
+//                    }
+//
+//                }
+//                if (rowNumber == verticalArraySize && columnNumber != 0) {
+//                    horizontalIndex = columnNumber - 1;
+//                    rowNumber--;
+//                    break;
+//
+//                }
+//                if (columnNumber == 0 && firstTime) {
+//
+//                    int newRowNumber = rowNumber - 2;
+//                    int rownumberGood = rowNumber - 1;
+//                    String key1 = "" + rownumberGood + columnNumber;
+//                    int keyInt1 = Integer.parseInt(key1);
+//                    if(!keysList.contains(keyInt1)){
+//                        rowNumber = newRowNumber;
+//                        verticalIndex = rowNumber;
+//                        horizontalIndex = 0;
+//                        firstTime = false;
+//                        break;
+//                    }
+//
+//
+//                }
--- MainTest.java
+++ MainTest.java
@@ -29,6 +29,17 @@ class MainTest {
         int[] r = {1, 2, 3, 9, 4, 1, 4, 3, 2, 1, 7, 4, 5, 6, 9, 8};
         test(array, r);
     }
+
+    @Test
+    public void SnailTest3() {
+        int[][] array
+                = {{1, 2, 3, 9},
+                {4, 5, 6, 4},
+                {7, 8, 9, 1},
+                {1, 2, 3, 4}};
+        int[] r = {1, 2, 3, 9, 4, 1, 4, 3, 2, 1, 7, 4, 5, 6, 9, 8};
+        test(array, r);
+    }
     public String int2dToString(int[][] a) {
         return Arrays.stream(a).map(row -> Arrays.toString(row)).collect(joining("\n"));
     }
